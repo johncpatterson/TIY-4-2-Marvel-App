4c52968b81c9a7dbf93e60d089d2e5594ad8c384
[John Patterson] <pattersonjc1@gmail.com>
2016-11-21 19:27:35 -0500

    got random images via API working and made some small style changes

--- js/getcharacters.js
+++ js/getcharacters.js
@@ -1,55 +1,37 @@
 jQuery(function() {
 
+   $("body").on("click", '#moreinfobutton', function() {
+      $(this).parent().parent().parent().find(".showMoreStuff").toggleClass("hide");
+   })
 
-    $("body").on("click", '#moreinfobutton', function() {
-        $(this).parent().parent().parent().find(".showMoreStuff").toggleClass("hide");
+   getCharacters();
 
-    })
-
-
-    getCharacters();
-
-    function getCharacters() {
-        $("#putCharactersHerePage").html("");
-        $.ajax({
-            url: `https://gateway.marvel.com:443/v1/public/characters?&apikey=50d21a7f18276125689a2e2f1c9bcd8e`,
-            method: 'GET',
-            success: function successHandler(featuredCharacters) {
-                console.log(featuredCharacters);
-                featuredCharacters.data.results.forEach(function(Character) {
-                    var characterID = Character.id;
-                    var series = Character.series.items[0];
-                    console.log(series);
-                    $("#putCharactersHerePage").append(`
-                        <div>
-                                    <div class="row">
-                                            <div class="col-md-6"><img src="${Character.thumbnail.path}.${Character.thumbnail.extension}" class="img-responsive"></div>
-                                            <div class="col-md-4 comic_info">${Character.name}</div>
-                                            <div class="col-md-2 comic_info"><button data-id="${characterID}" class="infoButton btn btn-lg btn-default" id="moreinfobutton">Get more info</button></div>
-                                    </div>
-                                    <div class="row hide showMoreStuff">
-                                            <div class="col-md-6 comic_info">${Character.description}</div>
-                                            <div class="col-md-6 comic_info">${series}</div>
-                                    </div>
-                        </div>
-
-                        `)
-
-                })
-            },
-        });
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-    // end of jQuery function tag:
-});
+   function getCharacters() {
+      $("#putCharactersHerePage").html("");
+      $.ajax({
+         url: `https://gateway.marvel.com:443/v1/public/characters?&apikey=50d21a7f18276125689a2e2f1c9bcd8e`,
+         method: 'GET',
+         success: function successHandler(featuredCharacters) {
+            console.log(featuredCharacters);
+            featuredCharacters.data.results.forEach(function(Character) {
+               var characterID = Character.id;
+               var series = Character.series.items[0];
+               console.log(series);
+               $("#putCharactersHerePage").append(`
+                      <div>
+                          <div class="row">
+                                <div class="col-md-6"><img src="${Character.thumbnail.path}.${Character.thumbnail.extension}" class="img-responsive character-image-large"></div>
+                                <div class="col-md-4 comic_info">${Character.name}</div>
+                                <div class="col-md-2 comic_info"><button data-id="${characterID}" class="infoButton btn btn-lg btn-default" id="moreinfobutton">Get more info</button></div>
+                          </div>
+                          <div class="row hide showMoreStuff">
+                                <div class="col-md-6 comic_info">${Character.description}</div>
+                                <div class="col-md-6 comic_info">${series}</div>
+                          </div>
+                      </div>
+                      `)
+            })
+         },
+      });
+   }
+}); // end of jQuery function tag:
--- js/main.js
+++ js/main.js
@@ -1,57 +1,55 @@
 jQuery(function() {
 
+   getCharacters();
+   getComics();
 
-    getCharacters();
-    getComics();
+   function getCharacters() {
+      $("#putCharactersHere").html("");
+      $.ajax({
+         url: `https://gateway.marvel.com:443/v1/public/characters?&apikey=50d21a7f18276125689a2e2f1c9bcd8e`,
+         method: 'GET',
+         success: function successHandler(featuredCharacters) {
+            console.log(featuredCharacters);
+            var randomCharacters1 = _.sample(featuredCharacters.data.results);
+            var randomCharacters2 = _.sample(featuredCharacters.data.results);
+            var randomCharacters3 = _.sample(featuredCharacters.data.results);
+            let newCharactersArray = [randomCharacters1, randomCharacters2, randomCharacters3];
+            newCharactersArray.forEach(function(Character) {
 
+               $("#putCharactersHere").append(`
+                        <div class="col-md-4"><img src="${Character.thumbnail.path}.${Character.thumbnail.extension}" class="img-responsive character-image-small"></div>
+                    `)
+            })
+         },
+      });
+   }
 
-    function getCharacters() {
-        $("#putCharactersHere").html("");
-        $.ajax({
-            url: `https://gateway.marvel.com:443/v1/public/characters?limit=3&apikey=50d21a7f18276125689a2e2f1c9bcd8e`,
-            method: 'GET',
-            success: function successHandler(featuredCharacters) {
-                console.log(featuredCharacters);
-                var arrayOfCharacters = featuredCharacters.data.results.forEach(function(Character) {
+   function getComics() {
+      $("#putComicsHere").html("");
+      $.ajax({
+         url: `https://gateway.marvel.com:443/v1/public/comics?&apikey=50d21a7f18276125689a2e2f1c9bcd8e`,
+         method: 'GET',
+         success: function successHandler(featuredComics) {
+            console.log(featuredComics);
+            var randomComics1 = _.sample(featuredComics.data.results);
+            var randomComics2 = _.sample(featuredComics.data.results);
+            var randomComics3 = _.sample(featuredComics.data.results);
+            let newComicsArray = [randomComics1, randomComics2, randomComics3];
+            newComicsArray.forEach(function(Comic) {
 
-                    // var random = arrayOfCharacters[Math.floor(Math.random() * arrayOfCharacters.length)];   <----- attempt at random number generation.  
-                    // I know I need to generate a random number then use that number to pick from the array that is returned from Marvel, just not sure how. 
-
-                    $("#putCharactersHere").append(`
-                               <div class="col-md-4"><img src="${Character.thumbnail.path}.${Character.thumbnail.extension}" class="img-responsive"></div>
-                        `)
-
-                })
-            },
-        });
-    }
-
-
-    function getComics() {
-        $("#putComicsHere").html("");
-        $.ajax({
-            url: `https://gateway.marvel.com:443/v1/public/comics?limit=3&apikey=50d21a7f18276125689a2e2f1c9bcd8e`,
-            method: 'GET',
-            success: function successHandler(featuredComics) {
-                console.log(featuredComics);
-                featuredComics.data.results.forEach(function(Comic) {
-
-                    $("#putComicsHere").append(`
-                               <div class="col-md-4"><img src="${Comic.thumbnail.path}.${Comic.thumbnail.extension}" class="img-responsive"></div>
-                        `)
-
-                })
-            },
-        });
-    }
-
-
+               $("#putComicsHere").append(`
+                           <div class="col-md-4"><img src="${Comic.thumbnail.path}.${Comic.thumbnail.extension}" class="img-responsive comic-image-small"></div>
+                    `)
+            })
+         },
+      });
+   }
 
 
 
 
 
-    // end of jQuery function tag:
+
+
+   // end of jQuery function tag:
 });
-
-
